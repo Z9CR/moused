5bce7257955c7f1d01edec163eec559ceb2e6f57
nekeri <nekerihere@gmail.com>
2026-07-23 23:34:08 +0800

    Linux support

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 cmake -B ./build 
 cmake --build ./build
-./build/moused
+sudo ./build/moused
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <definitions.hpp>
 #include <adapter.hpp>
 
+// debug tools
+/*
+#ifdef _WIN32
+#include <windows.h>
+#define sleep(x) Sleep(1000 * x)
+#else
+#include <unistd.h>
+#endif
+*/
+
 int main() {
     
     return 0;
