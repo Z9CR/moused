a86b4eb931eec584bb9ef8803a3187f7aa6ad07f
nekeri <nekerihere@gmail.com>
2026-07-24 00:55:31 +0800

    privilege attention in Unix(like)s

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
+#include <cstdio>
 #include <definitions.hpp>
 #include <adapter.hpp>
+
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+#include <unistd.h>
+#endif
 
 // debug tools
 /*
@@ -12,6 +17,14 @@
 */
 
 int main() {
+    // the program must have privilege in Linux and BSDs to RW /dev/uinput(Linux) or /dev/wsmouse(BSDs)
+    #if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+    if (geteuid() != 0) {
+        fprintf(stderr, "moused: root required (need write access to /dev/uinput). "
+                        "Run with: sudo moused\n");
+        return 1;
+    }
+    #endif
     
     return 0;
 }
