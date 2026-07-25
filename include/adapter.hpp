c450762dab5abdf88778b74c1f86b0f4fc64963d
NeKeri <nekerihere@gmail.com>
2026-07-25 20:35:42 +0800

    class+static method -> namespace + func

--- include/adapter.hpp
+++ include/adapter.hpp
@@ -4,38 +4,37 @@
 #pragma once
 #include <definitions.hpp>
 
-class mouse
+namespace mouse
 {
-public:
     /// @brief translate cursor
     /// @param a the angle away from x-
     /// @param distant how many PXs will cursor translate
-    static void translate(angle a, int distant);
+    void translate(angle a, int distant);
 
     /// @brief translate cursor
     /// @param a the angle away from x-
     /// @param distant how many PXs will cursor translate
-    static void translate(angle a, int distant, unsigned int time_ms);
+    void translate(angle a, int distant, unsigned int time_ms);
 
     /// @brief move the mouse to (x, y) (right = x+, down = y +)
     /// @param x: how many PXs away from left boarder
     /// @param y: how many PXs away from up boarder
-    static void move_to(unsigned int x, unsigned int y, unsigned int time_ms);
+    void move_to(unsigned int x, unsigned int y, unsigned int time_ms);
 
     /// @brief move the mouse to (x, y) (right = x+, down = y +)
     /// @param x: how many PXs away from left boarder
     /// @param y: how many PXs away from up boarder
-    static void move_to(unsigned int x, unsigned int y);
+    void move_to(unsigned int x, unsigned int y);
 
     /// @brief click the `btn`
-    static void click(mouse_btns btn);
+    void click(mouse_btns btn);
 
     /// @brief press the `btn`
-    static void press(mouse_btns btn);
+    void press(mouse_btns btn);
 
     /// @brief release the pressed `btn`
-    static void release(mouse_btns btn);
+    void release(mouse_btns btn);
 
     /// @brief routate the MMB for `scale`*Delta
-    static void wheel(wheel_rotations rotation, double scale);
+    void wheel(wheel_rotations rotation, double scale);
 };
