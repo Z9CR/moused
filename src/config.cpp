02197f0cd01aab0f3e70335650e25eb53e2ecf5b
NeKeri <nekerihere@gmail.com>
2026-07-26 12:40:57 +0800

    based the ui

--- src/config.cpp
+++ src/config.cpp
@@ -1,8 +1,8 @@
 #include <config.hpp>
 
-const int mainwindow_height = 400;
+const int mainwindow_width = 480;
 
-const int mainwindow_width = 300;
+const int mainwindow_height = 320;
 
 const char* cfg_path;
 
