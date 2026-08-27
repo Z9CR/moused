ac05b6f0004bcdea1dec75a006b57f30bbf73205
keri9 <keri9@fbsd.mshome.net>
2026-08-27 18:14:29 +0800

    chore: rm env echo in debug.sh

--- debug/debug.sh
+++ debug/debug.sh
@@ -3,10 +3,6 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
-echo $XDG_SESSION_TYPE
-echo $WAYLAND_DISPLAY
 cmake -B "$ROOT_DIR/build"
 cmake --build "$ROOT_DIR/build"
 "$ROOT_DIR/build/moused"
-echo $XDG_SESSION_TYPE
-echo $WAYLAND_DISPLAY
